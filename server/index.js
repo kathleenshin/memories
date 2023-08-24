935e8983519b2472435374c483bc320fb95a168d
kathleen <kathleen.n.shin@gmail.com>
2023-08-24 10:42:33 -0700

    install dotenv, add mongodb atlas environment variable, connect to server successful

--- server/index.js
+++ server/index.js
@@ -2,4 +2,29 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = process.env.ATLAS_URI;
+const PORT = process.env.PORT || 5000;
+
+// useFindAndModify is deprecated
+// mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+//     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+//     .catch((error) => console.log(error.message));
+
+// mongoose.set('useFindAndModify', false);
+
+mongoose.connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
